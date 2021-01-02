e10602862f089befa0172049ce3a3b7e01740f62
Nguyen Ba Duc <1760286@student.hcmus.edu.vn>
2021-01-02 21:10:44 +0700

    Adding customer data table migration

---
+++ database/migrations/2021_01_02_135824_create_customer_data_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_data', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->primary('user_id');
+            $table->string('phone');
+            $table->string('ship_address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_data');
+    }
+}
