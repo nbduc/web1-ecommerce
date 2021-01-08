797b48e21694edb73f89bcb4873c95303ef8744c
Tuan Dev <quoctuando030712@gmail.com>
2021-01-09 04:47:51 +0700

    show create product form

--- resources/views/admin/products/includes/form.blade.php
+++ resources/views/admin/products/includes/form.blade.php
@@ -1,13 +1,50 @@
 @csrf
+
 <div class="form-group">
     <label for="name">Name</label>
-    <input class="form-control @error('name') is-invalid @enderror" type="text" placeholder="Example: Dell Gaming G7 7590" name="name" required
+    <input class="form-control @error('name') is-invalid @enderror" type="text" placeholder="Example: Iphone 12 pro max" name="name" required
         value="{{ old('name') }}@isset($product){{ $product->name }}@endisset">
     @error('name')
     <div class="invalid-feedback">
         {{ $message }}
     </div>
     @enderror
+</div>
+<div class="form-group">
+    <label for="price">Price</label>
+    <input class="form-control @error('name') is-invalid @enderror" type="text" placeholder="Example: 21.990.000" name="price" >
+</div>
+<div class="form-group">
+    <label for="in_stock">In Stock(Max:1000)</label>
+    <input class="form-control @error('name') is-invalid @enderror" type="number"  name="in_stock" min="1" max="1000">
+</div>
+<div class="form-group">
+    <label for="display">Display</label>
+    <input class="form-control @error('name') is-invalid @enderror" type="text" placeholder="Example: Retina" name="display" >
+</div>
+<div class="form-group">
+    <label for="front_camera">Front Camera</label>
+    <input class="form-control @error('name') is-invalid @enderror" type="text" placeholder="Example: 12MP" name="front_camera" >
+</div>
+<div class="form-group">
+    <label for="rear_camera">Rear Camera</label>
+    <input class="form-control @error('name') is-invalid @enderror" type="text" placeholder="Example: 12MP" name="rear_camera" >
+</div>
+<div class="form-group">
+    <label for="storage">Storage</label>
+    <input class="form-control @error('name') is-invalid @enderror" type="text" placeholder="Example: 32GB" name="storage" >
+</div>
+<div class="form-group">
+    <label for="os">OS</label>
+    <input class="form-control @error('name') is-invalid @enderror" type="text" placeholder="Example: ios 14" name="os" >
+</div>
+<div class="form-group">
+    <label for="description">Description</label>
+    <textarea class="form-control @error('name') is-invalid @enderror" id="exampleFormControlTextarea1" rows="3" name="description"></textarea>
+</div>
+<div class="form-group">
+    <label for="feature_img">Feature Img</label>
+    <input type="file" class="form-control-file @error('name') is-invalid @enderror" id="exampleFormControlFile1" name="feature_img">
 </div>
 <div id="editor"></div>
 <br>
@@ -22,7 +59,7 @@
 <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 <script>
     var toolbarOptions = [
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],  
         ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
         ['blockquote', 'code-block'],
         [{ 'list': 'ordered'}, { 'list': 'bullet' }],
