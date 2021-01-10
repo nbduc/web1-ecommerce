5d9687c62f851a3944da359700ba6a0226f44353
Tuan Dev <quoctuando030712@gmail.com>
2021-01-11 04:57:02 +0700

    edit data factori and add img of product

--- database/factories/ProductImagesFactory.php
+++ database/factories/ProductImagesFactory.php
@@ -22,7 +22,19 @@ class ProductImagesFactory extends Factory
     public function definition()
     {
         return [
-            'url' => '/storage/images/'.$this->faker->image('public/storage/images', 480, 480, null, false),
+            //'url' => '/storage/images/'.$this->faker->image('public/storage/images', 480, 480, null, false),
+            'url' => $this->faker->randomElement([
+                '4659.jpg',
+                '151021-phones-review-hands.jpg',
+                '153887-phones-review-nokia-83.jpg',
+                'feature-9.jpg',
+                'images.jpg',
+                'IMG_1979-scaled.jpg',
+                'iphone-12-pro-512gb-281120-1212470.jpg',
+                'maxresdefault-3.jpg',   
+                'samsung-galaxy-s20.jpg',
+                'Vivo-V20-Pro-test.jpg',
+            ]),
         ];
     }
 }
